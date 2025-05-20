f603f5d44a10da9d4977d343bdf317de91c09e0f
pinstinct <contact@limhm.dev>
2025-05-20 11:19:00 +0900

    자동 포매팅 테스트

--- user/application/user_service.py
+++ user/application/user_service.py
@@ -26,8 +26,11 @@ class UserService:
         self.crypto = Crypto()
 
     def create_user(
-        self, name: str, email: str, password: str, memo: str | None = None,
-
+        self,
+        name: str,
+        email: str,
+        password: str,
+        memo: str | None = None,
     ):
         _user = None  # 데이터베이스에서 찾은 유저
 
