0582bfe5788dc11228d4a841dbd05a1dc5ec7ee8
pinstinct <contact@limhm.dev>
2025-05-22 13:51:35 +0900

    db 구성 환경 정리 및 rabbitMQ 추가

--- script/db.sh
+++
@@ -1,1 +0,0 @@
-docker run --name mysql-local -p 3306:3306/tcp -e MYSQL_ROOT_PASSWORD=test -d mysql:8
---
+++ script/init/init.sql
@@ -0,0 +1,3 @@
+-- 스키마 생성
+CREATE SCHEMA IF NOT EXISTS `fastapi-ca`;
+CREATE SCHEMA IF NOT EXISTS `messaging`;
